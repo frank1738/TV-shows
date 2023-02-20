0d7f1f70aeb8f269fa3c3660a2888141fadcdd41
frank1738 <franklineosoro08@gmail.com>
2023-02-20 21:13:35 +0300

    add setup files

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyBSkE7ML9WIzlrQq65JvSyI96qdfjs_i8g',
+  authDomain: 'tv-shows-774a2.firebaseapp.com',
+  projectId: 'tv-shows-774a2',
+  storageBucket: 'tv-shows-774a2.appspot.com',
+  messagingSenderId: '820540936912',
+  appId: '1:820540936912:web:cd62c3660f7d30550a04d5',
+};
