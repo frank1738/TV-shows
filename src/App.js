49c675dee6658971e3933942e53548453c0de47e
frank1738 <franklineosoro08@gmail.com>
2023-03-01 12:30:51 +0300

    create user slice in readux

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
 import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './components/Login';
 import { auth } from './firebase';
+import { logOut, login } from './features/userSlice';
 import './App.css';
 import HomeScreen from './components/HomeScreen';
 
 function App() {
   const user = false;
+  const dispatch = useDispatch();
   useEffect(() => {
     const unsubscribe = auth.onAuthStateChanged((userAuth) => {
       if (userAuth) {
-        console.log(userAuth);
+        dispatch(
+          login({
+            uid: userAuth.uid,
+            email: userAuth.email,
+          })
+        );
       } else {
+        dispatch(logOut);
       }
     });
     return unsubscribe;
